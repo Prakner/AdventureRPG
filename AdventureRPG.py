affbc8feaab56fc82b2a1a0c5252e3bccf9891ff
Prakner <zarehvart@gmail.com>
2023-09-01 12:07:38 -0500

    Battle Dmg Calculation

--- AdventureRPG.py
+++ AdventureRPG.py
@@ -329,8 +329,17 @@ x = adven_gen(None,None,None,None,None,None,None,None,None,None,5000000)
 for i in range(1,100):
     levelup(x)
 
-# Testing corner
-adven_list_append(adven_gen())
+def battle_dmg(attack:int, defense:int, attack_modifier:float = None, defense_modifier:float = None):
+    n = random.choice([-1,1])
+    if attack_modifier == None:
+        attack_modifier = 1.0
+    if defense_modifier == None:
+        defense_modifier = 1.0
+    return int(((attack_modifier)*(attack/2) - (defense_modifier)*(defense/4)) + n*(attack/2 - defense/4)/16)
+
+test1 = battle_dmg(x.strength, x.defense, None, 1.5)
+test2 = battle_dmg(x.strength, x.defense, 1.5) 
+test3 = battle_dmg(x.strength, x.defense)
 
 for adven in adventurer_list:
     fprint(str(adven))
